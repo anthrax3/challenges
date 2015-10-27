222e464ea7178e22de05f31630896632902ad240
b4x <b4x@users.noreply.github.com>
2015-10-27 22:54:02 +0100

    Create app.config.js

---
+++ js/app.config.js
@@ -0,0 +1,20 @@
+angular.module('challengesApp').config(function($locationProvider, $urlRouterProvider, $stateProvider){
+
+	$locationProvider.html5Mode(true);
+
+	$urlRouterProvider.otherwise('/');
+
+	$stateProvider
+
+	.state('main', {
+		url: '/',
+		templateUrl: '/views/main.html',
+		//controller: 'postsCtrl'
+	})
+
+	.state('00', {
+		url: '/00',
+		templateUrl: '/views/00.html'
+	})
+
+})
