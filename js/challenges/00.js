59bcf91e51018ba8b8741570f909755ec4b10b40
b4x <b4x@users.noreply.github.com>
2015-10-31 14:50:26 +0100

    Update 00.js

--- js/challenges/00.js
+++ js/challenges/00.js
@@ -1,1 +1,10 @@
-console.log('js loaded');
+$(function() {
+	$( "#name-male" ).click(function() {
+		console.log('#name-male clicked');
+  	$(".name-male").text("John");
+	});
+	$( "#name-female" ).click(function() {
+		console.log('#name-female clicked');
+  	$(".name-female").text("Mary");
+	});
+});
