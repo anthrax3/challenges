7333b76afef7b0505be37bffae3880375b2d3dc2
b4x <b4x@users.noreply.github.com>
2015-10-31 14:59:01 +0100

    Update 00.js

--- js/challenges/00.js
+++ js/challenges/00.js
@@ -1,7 +1,18 @@
 $(function() {
+	var namesMale = [
+	Adam, Barry, Colin,
+	Dean, Ethan, Fred
+	];
+	var namesFemale = [
+	Anna, Beth, Carol,
+	Daisy, Emily, Felicity
+	];
+	var randomName = function(arr) {
+		return arr[0];
+	}
 	$( "#name-male" ).click(function() {
 		console.log('#name-male clicked');
-  	$(".name-male").text("John");
+  	$(".name-male").text(randomName(namesMale));
 	});
 	$( "#name-female" ).click(function() {
 		console.log('#name-female clicked');
