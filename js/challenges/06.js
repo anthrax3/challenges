d16dcfb2e381a0a5588fababff4d8aea9d847d83
b4x <b4x@users.noreply.github.com>
2015-11-02 18:04:23 +0100

    Update 06.js

--- js/challenges/06.js
+++ js/challenges/06.js
@@ -1,5 +1,37 @@
 $(function() {
 	$( ".fa" ).click(function() {
-	$( "#result" ).text("You chose " + $( this ).parent().text().toLowerCase());
+		var computerChoice = Math.random();
+		if (computerChoice <= 0.33) {
+		   computerChoice = 'rock';
+		} else if (computerChoice > 0.33 && 0.66) {
+		    computerChoice = 'paper';
+		} else {
+		    computerChoice = 'scissors';
+		}
+		var userChoice = $( this ).parent().text().toLowerCase();
+		var compare = function(choice1, choice2) {
+	    if (choice1 === choice2) {
+	        return 'The result is a tie!';
+	    } else if (choice1 === 'rock') {
+	    	if (choice2 === 'scissors') {
+	      	return 'rock wins';    
+	      } else {
+	        return 'paper wins';   
+	      }
+	    } else if (choice1 === 'paper') {
+	    	if (choice2 === 'rock') {
+	      	return 'paper wins';   
+	      } else {
+	      	return 'scissors wins';
+	      }
+	    } else if (choice1 === 'scissors') {
+	    	if (choice2 === 'rock') {
+	        return 'rock wins';
+	      } else {
+	      	return 'scissors wins';
+	      }
+	    }
+	  };
+		$( "#result" ).text("You chose " + userChoice + ". Computer chose " + computerChoice + "<br />" + compare(userChoice,computerChoice));
 	});
 });
