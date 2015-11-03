5a77510f08eecc6675d0b0da9ab21d788e509617
b4x <b4x@users.noreply.github.com>
2015-11-03 16:47:47 +0100

    Create 15.js

---
+++ js/challenges/15.js
@@ -0,0 +1,10 @@
+$(function() {
+	function reverseStr() {
+		{
+			str = $( "#input " ).val();
+	    $( ".reverseStr" ).text(str + " reversed is " + str.split('').reverse().join(''));
+		}
+	$( "#reverseStr" ).click(function() 
+		reverseStr();
+	});
+});
