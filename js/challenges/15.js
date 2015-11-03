f9e07e01b1dffc508c4a0f399ffccbce4bd0c87d
b4x <b4x@users.noreply.github.com>
2015-11-03 16:50:48 +0100

    Update 15.js

--- js/challenges/15.js
+++ js/challenges/15.js
@@ -1,10 +1,9 @@
 $(function() {
 	function reverseStr() {
-		{
-			str = $( "#input " ).val();
-	    $( ".reverseStr" ).text(str + " reversed is " + str.split('').reverse().join(''));
-		}
-	$( "#reverseStr" ).click(function() 
+		str = $( "#input " ).val();
+    $( ".reverseStr" ).text(str + " reversed is " + str.split('').reverse().join(''));
+	}
+	$( "#reverseStr" ).click(function() {
 		reverseStr();
 	});
 });
