ae12e7afe90bce399636fb42bb1da43bf4b192f1
b4x <b4x@users.noreply.github.com>
2015-11-05 09:54:57 +0100

    Update 07.js

--- js/challenges/07.js
+++ js/challenges/07.js
@@ -93,61 +93,61 @@ function drawCanvas() {
     // reset the canvas and set basic styles
     canvas.width = canvas.width;
     c.lineWidth = 10;
-    c.strokeStyle = '#A52A2A';
+    c.strokeStyle = 'green';
     c.font = 'bold 24px Optimer, Arial, Helvetica, sans-serif';
     c.fillStyle = 'red';
     // draw the ground
-    //drawLine(c, [20,190], [180,190]);
+    drawLine(c, [20,190], [180,190]);
     // start building the gallows if there's been a bad guess
-
-    // draw the gallows
-
-    // create the upright
-    drawLine(c, [30,185], [30,10]);
-
-    // create the arm of the gallows
-    c.lineTo(150,10);
-    c.stroke();
-
     if (badGuesses > 0) {
-	    c.strokeStyle = 'black';
-	    c.lineWidth = 3;
-	    // draw rope
-	    drawLine(c, [145,15], [145,30]);
-	    // draw head
-	    c.beginPath();
-	    c.moveTo(160, 45);
-	    c.arc(145, 45, 15, 0, (Math.PI/180)*360);
-	    c.stroke();
-      if (badGuesses > 1) {
-        // draw body
-        drawLine(c, [145,60], [145,130]);
-      }
-      if (badGuesses > 2) {
-      	// draw left arm
-        drawLine(c, [145,80], [110,90]);
-      }
-      if (badGuesses > 3) {
-        // draw right arm
-        drawLine(c, [145,80], [180,90]);
-      }
-      if (badGuesses > 4) {
-        // draw left leg
-        drawLine(c, [145,130], [130,170]);
-      }
-      if (badGuesses > 5) {
-        // draw right leg and end game
-        drawLine(c, [145,130], [160,170]);
-        c.fillText('Game over', 45, 110);
-        // remove the alphabet pad
-        letters.innerHTML = '';
-        // display the correct answer
-        // need to use setTimeout to prevent race condition
-        setTimeout(showResult, 200);
-        // increase score of lost games
-        // display the score after two seconds
-        // code to be added later
-      }
+        // create the upright
+        c.strokeStyle = '#A52A2A';
+        drawLine(c, [30,185], [30,10]);
+        if (badGuesses > 1) {
+            // create the arm of the gallows
+            c.lineTo(150,10);
+            c.stroke();
+        }
+        if (badGuesses > 2) {
+            c.strokeStyle = 'black';
+            c.lineWidth = 3;
+            // draw rope
+            drawLine(c, [145,15], [145,30]);
+            // draw head
+            c.beginPath();
+            c.moveTo(160, 45);
+            c.arc(145, 45, 15, 0, (Math.PI/180)*360);
+            c.stroke(); 
+        }
+        if (badGuesses > 3) {
+            // draw body
+            drawLine(c, [145,60], [145,130]);
+        }
+        if (badGuesses > 4) {
+            // draw left arm
+            drawLine(c, [145,80], [110,90]);
+        }
+        if (badGuesses > 5) {
+            // draw right arm
+            drawLine(c, [145,80], [180,90]);
+        }
+        if (badGuesses > 6) {
+            // draw left leg
+            drawLine(c, [145,130], [130,170]);
+        }
+        if (badGuesses > 7) {
+            // draw right leg and end game
+            drawLine(c, [145,130], [160,170]);
+            c.fillText('Game over', 45, 110);
+            // remove the alphabet pad
+            letters.innerHTML = '';
+            // display the correct answer
+            // need to use setTimeout to prevent race condition
+            setTimeout(showResult, 200);
+            // increase score of lost games
+            // display the score after two seconds
+            // code to be added later
+        }
     }
     // if the word has been guessed correctly, display message,
     // update score of games won, and then show score after 2 seconds
