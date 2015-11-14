0c21cd1c1b8e601afc419aab763665422ccb5671
b4x <b4x@users.noreply.github.com>
2015-11-14 21:44:16 +0100

    Update 27.js

--- js/challenges/27.js
+++ js/challenges/27.js
@@ -1,31 +1,29 @@
 $(function() {
 
 	function recursion(n) {
-	var recTimeA = performance.now();
-    if(n <= 2) {
-				//$( "#resultRec" ).append(1 + '<br />');
-        return 1;
-    } else {
-$( "#resultRec" ).append(recursion(n - 1) + recursion(n - 2) + '<br />');
-    }
-
-    var recTimeB = performance.now();
-    $( "#resultRec" ).append( "It took " + (recTimeB - recTimeA) + " ms." );
+		var recTimeA = performance.now();
+	  if(n <= 2) {
+			$( "#resultRec" ).append('mario' + '<br />');
+	    return 1;
+	  } else {
+	  	return this.recursion(n - 1) + this.recursion(n - 2);
+			$( "#resultRec" ).append('luigi' + '<br />');
+	  }
+		var recTimeB = performance.now();
+	  $( "#resultRec" ).append( "It took " + (recTimeB - recTimeA) + " ms." );
 	}
 
 	function iteration(n) {
-	var itTimeA = performance.now();
+		var itTimeA = performance.now();
 	  var a = 0, b = 1, f = 1;
 	  for(var i = 2; i <= n; i++) {
-	      f = a + b;
-	      a = b;
-	      b = f;
-				$( "#resultIt" ).append(f + '<br />');
+	    f = a + b;
+	    a = b;
+	    b = f;
+			$( "#resultIt" ).append(f + '<br />');
 	  }
-        $( "#resultIt" ).append('mario');
-     var itTimeB = performance.now();
+    var itTimeB = performance.now();
     $( "#resultIt" ).append( "It took " + (itTimeB - itTimeA) + " ms." );
-
 	}
 
 	$( "#genFib" ).click(function() {
